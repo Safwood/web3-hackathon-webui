018de555f01eb8709c69493216ec9ab480491c20
safwood <naste-f@yandex.ru>
2023-12-02 11:16:34 +0300

    add redux

---
+++ src/redux/auth/auth.reducer.ts
@@ -0,0 +1,59 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { State } from "redux/store";
+import { LoginData } from "utils/types";
+
+export type AuthState = {
+  authStatus: "initial" | "loading" | "error" | "resolved";
+  isUserLoggedIn: boolean;
+  id: number | null;
+  username: string;
+  authErrorText: string;
+};
+
+const initialState = {
+  authStatus: "initial",
+  isUserLoggedIn: false,
+  id: null,
+  username: "",
+  authErrorText: ""
+} as AuthState;
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logIn: (state, action: PayloadAction<Required<LoginData>>) => {
+      state.isUserLoggedIn = true;
+      state.authStatus = "resolved";
+      if (action.payload) {
+        state.authErrorText = "";
+        state.id = action.payload.id;
+        state.username = action.payload.username;
+      }
+    },
+    logOut: (state) => {
+      state.isUserLoggedIn = false;
+      state.authStatus = "initial";
+    },
+    setAuthErrorText: (state, action: PayloadAction<Required<string>>) => {
+      if (action.payload) {
+        state.authErrorText = action.payload;
+      }
+    },
+    setAuthErrorStatus: (
+      state,
+      action: PayloadAction<
+        Required<"initial" | "loading" | "error" | "resolved">
+      >
+    ) => {
+      state.authStatus = action.payload;
+    }
+  },
+  extraReducers: (_builder) => {}
+});
+
+export const authSelector = (state: State) => state.auth;
+
+export default authSlice.reducer;
+export const { setAuthErrorText, logOut, logIn, setAuthErrorStatus } =
+  authSlice.actions;
